704240f3ecc3fe511b160a357d35acf7cc4bb058
Gleb-Kolonin <topsecretway@yandex.ru>
2022-11-14 02:38:00 +0300

    Удаление папок с скриптами

--- Handling_data/lesson_1/2 - Stealth Indicator.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-# Создаем окно браузера Firefox под управлением Selenium
-driver = webdriver.Chrome(executable_path=r'D:\Soft\SeleniumWebDrivers\chromedriver-106.exe')
-
-# Отправляем драйверу команду на загрузку урла
-#driver.get("http://ithub.role.ru/news.html")
-driver.get("https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html")
